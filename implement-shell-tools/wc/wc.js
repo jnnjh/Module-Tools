db6d0083beefd42b380ecbdbab64bcb9d78e0cbd
jnnjh <joannejhoieacar@gmail.com>
2026-04-07 09:38:59 +0200

    added -l -w -c flags

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -1,5 +1,19 @@
 const fs = require("fs");
-const files = process.argv.slice(2);
+const args = process.argv.slice(2);
+let mode = "all";
+let files = [];
+
+for(let arg of args) {
+    if(arg === "-l") {
+        mode = "lines";
+    } else if(arg === "-w") {
+        mode = "words";
+    } else if(arg === "-c") {
+        mode = "bytes";
+    } else {
+        files.push(arg);
+    }
+}
 
 for(let file of files) {
     const content = fs.readFileSync(file, "utf-8");
@@ -7,5 +21,13 @@ for(let file of files) {
     const words = content.trim().split(/\s+/).length;
     const bytes = Buffer.byteLength(content, "utf-8");
 
-    console.log(`${lines} ${words} ${bytes} ${file}`);
+    if(mode === "lines") {
+        console.log(`${lines} ${file}`);
+    } else if(mode === "words") {
+        console.log(`${words} ${file}`);
+    } else if (mode === "bytes") {
+        console.log(`${bytes} ${file}`);
+    } else {
+        console.log(`${lines} ${words} ${bytes} ${file}`);
+    }
 }
