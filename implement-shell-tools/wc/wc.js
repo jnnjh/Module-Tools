327d65857d9fc3d0b271e88564e7ad91552f6651
jnnjh <joannejhoieacar@gmail.com>
2026-04-07 09:36:10 +0200

    wc for multiple files

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
-const file = process.argv[2];
-const content = fs.readFileSync(file, "utf-8");
-const lines = content.split("\n").length;
-const words = content.trim().split(/\s+/).length;
-const bytes = Buffer.byteLength(content, "utf-8");
+const files = process.argv.slice(2);
 
-console.log(`${lines} ${words} ${bytes} ${file}`);
+for(let file of files) {
+    const content = fs.readFileSync(file, "utf-8");
+    const lines = content.split("\n").length;
+    const words = content.trim().split(/\s+/).length;
+    const bytes = Buffer.byteLength(content, "utf-8");
+
+    console.log(`${lines} ${words} ${bytes} ${file}`);
+}
