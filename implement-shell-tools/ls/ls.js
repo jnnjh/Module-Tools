7aa7b81d3dd513e961821b94b9fcb67d3895af21
jnnjh <joannejhoieacar@gmail.com>
2026-04-07 09:29:55 +0200

    added -a flag to show hidden files

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,15 +1,21 @@
 const fs = require("fs");
 const args = process.argv.slice(2);
-
+let showAll = false;
 let dir = ".";
 
 for(let arg of args) {
-    if(!arg.startsWith("-")) {
+    if(arg === "-a") {
+        showAll = true;
+    } else if(!arg.startsWith("-")) {
         dir = arg;
     }
 }
 
-const files = fs.readdirSync(dir);
+let files = fs.readdirSync(dir);
+
+if(!showAll) {
+    files = files.filter(file => !file.startsWith("."));
+}
 
 for(let file of files) {
     console.log(file);
