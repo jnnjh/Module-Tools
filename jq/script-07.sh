2f85bc8fe74fcca701960f4b062215d6ee4ca936
jnnjh <joannejhoieacar@gmail.com>
2026-04-07 11:18:30 +0200

    answered jq exercises

--- jq/script-07.sh
+++ jq/script-07.sh
@@ -6,3 +6,13 @@ set -euo pipefail
 # TODO: Write a command to output just the names of each player along with the score from their last attempt.
 # Your output should contain 6 lines, each with one word and one number on it.
 # The first line should be "Ahmed 4" with no quotes.
+#!/bin/bash
+
+set -euo pipefail
+
+# The input for this script is the scores.json file.
+# TODO: Write a command to output just the names of each player along with the score from their first attempt.
+# Your output should contain 6 lines, each with one word and one number on it.
+# The first line should be "Ahmed 1" with no quotes.
+
+jq -r '.[] | [.name, .scores[-1]] | join(" ")' scores.json
